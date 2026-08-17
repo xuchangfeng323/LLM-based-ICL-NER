de5cec1e27b1fb74a873190c129719fa62294f51
xuchangfeng <xuchangfeng0323@163.com>
2026-08-17 16:15:38 +0800

    feat: add ollama model inference support
    add ollama client initialization and get_ollama_response function, support fallback to default model when target model not found

--- get_response.py
+++ get_response.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import ollama
+from ollama import Client, ChatResponse
 
 
 load_dotenv()
@@ -19,6 +21,7 @@ if "qwen_api_key"  in os.environ:
         api_key=os.environ["qwen_api_key"],
         base_url="https://zyapi.tuluo.top:8888/v1",
     )
+ollama_client = Client(host="http://localhost:11434")
 def get_deepseek_response(model_name="deepseek-v4-flash", system_prompt="", user_prompt="" ):
     
     messages = [{"role": "system", "content": system_prompt},
@@ -63,3 +66,23 @@ def get_qwen_response(model_name="Qwen3.6-35B-A3B-abliterated", system_prompt=""
         },
     )
     return response.choices[0].message.content
+def get_ollama_response(model_name="qwen3.8", system_prompt="", user_prompt="" ):
+    model_list=[m.model for m in ollama.list().models]
+    if model_name not in model_list:
+        model_name="qwen3.8"
+    messages = [{"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt}]
+
+    response:ChatResponse = ollama_client.chat(
+        model=model_name,
+        messages=messages,
+        options={
+            "temperature": 1,
+            "top_p": 1,
+            "think":False
+            
+        }
+    )
+    return response.message.content
+
+    
